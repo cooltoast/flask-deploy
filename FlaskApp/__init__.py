8943c6ef80a97ffe14c2bbdcbd0bd434d1df837f
cooltoast <mpatel678@gmail.com>
2014-06-30 21:22:36 +0000

    add flask demo with multiple pages

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,7 +1,33 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello World, I love Digital Ocean!"
-if __name__ == "__main__":
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('testtemplate.html', name=name) 
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+   	return 'post made' 
+    else:
+	return 'post not made'
+
+if __name__ == '__main__':
     app.run()
